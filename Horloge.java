3eb9eecc2364c209a9dbfb2bfeef48901f5bc76a
Falcand Gaetan <gfalcand@PC107-043-03.insa-lyon.fr>
2019-04-03 15:33:46 +0200

    Sisi la famille, les afficheurs bientôt dans les backs

--- Horloge.java
+++ Horloge.java
@@ -4,15 +4,31 @@ import javax.swing.*;
 
 public class Horloge {
     
-    public int temps;
+    private int temps;
+    private int score;
     
-    public Horloge(int tempsDonne){
+    
+    public Horloge(int tempsDonne, int scoreDonne){
         temps=tempsDonne;
+        score=scoreDonne;
     }
     
     public void dessine(Graphics g){
-        g.setFont(new Font("Latin", Font.PLAIN, 35));
-        String s = Integer.toString(temps)+" s";
-        g.drawString(s,100,100);
+            g.setFont(new Font("Latin", Font.PLAIN, 35));
+            String s1 = Integer.toString(temps)+" s";
+            g.drawString(s1,100,100);
+            String s2 = "SCORE : "+Integer.toString(score);
+            g.drawString(s2,300,300);
     }
+    
+    public void setTemps(int valeur){
+        this.temps = valeur;
+    }
+    
+    public void setScore(int valeur){
+        this.score = score;
+    }
+    
 }
+    
+
